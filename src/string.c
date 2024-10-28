ziyao233/loli-loader

commit a3d6dd8813771391e4165de45e97b84619614800

Add printf

src/string.c
@@ -1,0 +1,150 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/src/string.c
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#include <efidef.h>
+#include <stdarg.h>
+
+#include <string.h>
+
+size_t
+strlen(const char *p)
+{
+	size_t len = 0;
+	while (*p++)
+		len++;
+	return len;
+}
+
+char *
+strcpy(char *dst, const char *src)
+{
+	char *org = dst;
+	while (*src)
+		*(dst++) = *(src++);
+	*dst = 0;
+	return org;
+}
+
+size_t
+wcslen(const wchar_t *p)
+{
+	size_t len = 0;
+	while (*p++)
+		len++;
+	return len;
+}
+
+wchar_t *
+wcscpy(wchar_t *dst, wchar_t *src)
+{
+	wchar_t *org = dst;
+	while (*src)
+		*(dst++) = *(src++);
+	*dst = 0;
+	return org;
+}
+
+size_t
+wcs2str(char *str, const wchar_t *wcs)
+{
+	size_t len = wcslen(wcs);
+	if (!str)
+		return len;
+
+	while (*wcs)
+		*(str++) = (char)*(wcs++);
+	*str = 0;
+
+	return len;
+}
+
+size_t
+str2wcs(wchar_t *wcs, const char *str)
+{
+	size_t len = wcslen(wcs);
+	if (!wcs)
+		return len;
+
+	while (*str)
+		*(wcs++) = (wchar_t)*(str++);
+	*wcs = 0;
+
+	return len;
+}
+
+static int
+itoa_n(char *out, unsigned long int n, int base)
+{
+	static const char digits[] = "0123456789abcdef";
+
+	out[0] = '0';
+
+	int len = 0;
+	while (n) {
+		out[len] = digits[n % base];
+		n /= base;
+		len++;
+	}
+
+	char tmp;
+	for (int i = 0; i < len / 2; i++) {
+		tmp = out[len - i - 1];
+		out[len - i - 1] = out[i];
+		out[i] = tmp;
+	}
+
+	return len;
+}
+
+void
+vsprintf(char *p, const char *format, va_list va)
+{
+	bool longValue;
+	int base = 10;
+	long int value;
+	while (*format)
+	switch (*format) {
+	case '%':
+		format++;
+		if (*format == '%') {
+			*(p++) = *(format++);
+			break;
+		}
+		if (*format == 'l') {
+			format++;
+			value = va_arg(va, long int);
+			longValue = 1;
+		} else {
+			value = va_arg(va, int);
+			longValue = 0;
+		}
+
+		switch (*format) {
+		case 'd':
+			if (value < 0) {
+				*(p++) = '-';
+				value = -value;
+			}
+			// fallthrough
+		case 'u':
+			base = 10;
+			break;
+		case 'x':
+			base = 16;
+			break;
+		}
+		format++;
+
+		p += itoa_n(p, longValue ? value : (value & 0xffffffff), base);
+		break;
+	default:
+		*(p++) = *(format++);
+		break;
+	}
+
+	*p = '\0';
+}

Makefile
@@ -16,7 +16,7 @@ CFLAGS		?= -ffreestanding -fno-stack-protector -fno-stack-check \
 CCASFLAGS	?= $(CFLAGS)
 LDFLAGS		=
 
-OBJS		= src/loli.o src/efi.o
+OBJS		= src/loli.o src/efi.o src/string.o src/interaction.o
 
 default: loli.efi
 

include/efidef.h
@@ -15,6 +15,7 @@ typedef unsigned long int uint64_t;
 typedef uint32_t uint_native;
 typedef uint8_t bool;
 typedef uint16_t wchar_t;
+typedef unsigned long int size_t;
 
 typedef void * Efi_Handle;
 typedef uint_native Efi_Status;

include/interaction.h
@@ -1,0 +1,14 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/interaction.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+#ifndef __LOLI_INTERACTION_H_INC__
+#define __LOLI_INTERACTION_H_INC__
+
+#include <efidef.h>
+
+void printf(const char *format, ...);
+
+#endif	// __LOLI_INTERACTION_H_INC__

include/stdarg.h
@@ -1,0 +1,21 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/vaarg.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#ifndef __LOLI_STDARG_H_INC__
+#define __LOLI_STDARG_H_INC__
+
+#if !defined(__clang__) && !defined(__GNUC__)
+#warning "Current va_arg implementation may not work on this compiler"
+#endif
+
+typedef __builtin_va_list	va_list;
+#define va_start(v, l)		__builtin_va_start(v, l)
+#define va_end(v)		__builtin_va_end(v)
+#define va_arg(v, t)		__builtin_va_arg(v, t)
+#define va_copy(d, s)		__builtin_va_copy(d, s)
+
+#endif	// __LOLI_STDARG_H_INC__

include/string.h
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/string.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#ifndef __LOLI_STRING_H_INC__
+#define __LOLI_STRING_H_INC__
+
+#include <efidef.h>
+#include <stdarg.h>
+
+size_t strlen(const char *p);
+char *strcpy(char *dst, const char *src);
+
+size_t wcslen(const wchar_t *p);
+wchar_t *wcscpy(wchar_t *dst, wchar_t *src);
+
+size_t wcs2str(char *str, const wchar_t *wcs);
+size_t str2wcs(wchar_t *wcs, const char *str);
+
+void vsprintf(char *p, const char *format, va_list va);
+
+#endif	// __LOLI_STRING_H_INC__

src/interaction.c
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/src/interaction.c
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#include <stdarg.h>
+#include <string.h>
+
+#include <efi.h>
+#include <eficall.h>
+#include <eficon.h>
+
+#include <interaction.h>
+
+void
+printf(const char *format, ...)
+{
+	va_list va;
+	va_start(va, format);
+
+	char buf[256];
+	vsprintf(buf, format, va);
+
+	wchar_t buf2[256];
+	str2wcs(buf2, buf);
+
+	efi_method(gST->conOut, outputString, buf2);
+
+	va_end(va);
+}

src/loli.c
@@ -8,6 +8,7 @@
 #include <efidef.h>
 #include <eficall.h>
 #include <efi.h>
+#include <interaction.h>
 
 Efi_Status
 _start(Efi_Handle imageHandle, Efi_System_Table *st)
@@ -16,6 +17,9 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 
 	efi_method(gST->conOut, outputString, L"Hello world\n");
 	efi_method(gST->conOut, outputString, L"Hello world2\n");
+
+	printf("Hello world %d %x %lx %u\n", -1, -1, 0xffffffffffffffff, -1);
+	printf("%x %d\n", 0x12345a5a, 1234);
 
 	return EFI_SUCCESS;
 }

commit 8f066bbe158ee418bac3937b0eecab96f9d3617f

Fix str2wcs implementation
Fixes: a3d6dd8 ("Add printf")
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/string.c
@@ -65,7 +65,7 @@ wcs2str(char *str, const wchar_t *wcs)
 size_t
 str2wcs(wchar_t *wcs, const char *str)
 {
-	size_t len = wcslen(wcs);
+	size_t len = strlen(str);
 	if (!wcs)
 		return len;
 
@@ -81,7 +81,10 @@ itoa_n(char *out, unsigned long int n, int base)
 {
 	static const char digits[] = "0123456789abcdef";
 
-	out[0] = '0';
+	if (n == 0) {
+		out[0] = '0';
+		return 1;
+	}
 
 	int len = 0;
 	while (n) {