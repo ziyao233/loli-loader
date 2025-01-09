af1674948dad16b35a50b002348a06b9a33e1d18
Yao Zi <ziyao@disroot.org>
2025-01-09 22:06:27 +0000

    main: Add skeleton
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- src/loli.c
+++ src/loli.c
@@ -17,6 +17,8 @@
 #include <extlinux.h>
 #include <misc.h>
 
+#define LOLI_CFG "loli.cfg"
+
 void
 printn(const char *p, size_t len)
 {
@@ -24,92 +26,141 @@ printn(const char *p, size_t len)
 		printf("%c", *(p++));
 }
 
+static char *
+get_pair(const char *cfg, const char *key)
+{
+	size_t len = 0;
+	const char *res = extlinux_get_value(cfg, key, &len);
+
+	if (!res)
+		return 0;
+
+	char *copy = malloc(len + 1);
+	strscpy(copy, res, len + 1);
+
+	return copy;
+}
+
+int
+retrieve_timeout(const char *cfg)
+{
+	char *res = get_pair(cfg, "timeout");
+	if (!res)
+		return 0;
+
+	int timeout = atou(res);
+	if (timeout < 0) {
+		pr_warn("invalid timeout \"%s\", use 0 instead\n", res);
+		timeout = 0;
+	}
+
+	free(res);
+
+	return timeout;
+}
+
+int
+wait_for_boot_entry(int timeout)
+{
+	printf("Enter entry number: ");
+	char *line = getline_timeout(timeout);
+
+	/* TODO: support "default" option */
+	if (!line)
+		return 0;
+
+	int entry = atou(line);
+	free(line);
+
+	return entry >= 0 ? entry : -1;
+}
+
+static const char *
+get_entry(const char *cfg, int index)
+{
+	const char *entry = extlinux_next_entry(cfg, NULL);
+
+	while (index--)
+		entry = extlinux_next_entry(NULL, entry);
+
+	return entry;
+}
+
 Efi_Status
 _start(Efi_Handle imageHandle, Efi_System_Table *st)
 {
 	efi_init(imageHandle, st);
 
-	efi_method(gST->conOut, outputString, L"Hello world\n");
-	efi_method(gST->conOut, outputString, L"Hello world2\n");
+	printf("loli bootloader (%s built)\n", __DATE__);
 
-	Efi_Loaded_Image_Protocol *loadedImage;
-	efi_handle_protocol(imageHandle, EFI_LOADED_IMAGE_PROTOCOL_GUID,
-			    &loadedImage);
-	printf("loaded image: %p\n", loadedImage);
+	int64_t cfgSize = file_get_size(LOLI_CFG);
+	if (cfgSize < 0)
+		panic("Can't retrive size of configuration");
 
-	printf("image base = %p, image size = %lu\n",
-	       loadedImage->imageBase, loadedImage->imageSize);
+	char *cfg = malloc(cfgSize + 1);
+	cfgSize = file_load(LOLI_CFG, (void **)&cfg);
+	if (cfgSize < 0)
+		panic("Can't load configuration");
 
-	printf("Hello world %d %x %lx %u\n", -1, -1, 0xffffffffffffffff, -1);
-	printf("%x %d\n", 0x12345a5a, 1234);
-	printf("%d %d\n", 0, atou("12345678"));
-
-	printf("Length of test.txt is %ld\n", file_get_size("test.txt"));
-	char *buf = NULL;
-	int64_t size = file_load("test.txt", (void **)&buf);
-	printf("test.txt content:\n\t");
-	printn(buf, size);
-	free(buf);
-
-	size = file_get_size("loli.cfg");
-	printf("Length of extlinux.conf is %ld\n", size);
-	buf = malloc(size + 1);
-	file_load("loli.cfg", (void **)&buf);
-	printf("loli.cfg content:\n");
-	printn(buf, size);
-	buf[size] = '\0';
-	printf("loli.cfg boot entries\n");
+	int timeout = retrieve_timeout(cfg);
 	int entryNum = 0;
-	for (const char *p = extlinux_next_entry(buf, NULL);
+	for (const char *p = extlinux_next_entry(cfg, NULL);
 	     p;
 	     p = extlinux_next_entry(NULL, p)) {
 		size_t namelen = 0;
+		const char *name = extlinux_get_value(cfg, "label", &namelen);
 
-		const char *name = extlinux_get_value(p, "label", &namelen);
-		printf("%d: ", entryNum + 1);
+		printf("%d: ", entryNum);
 		printn(name, namelen);
 		printf("\n");
 
 		entryNum++;
 	}
 
-	char *line = getline_timeout(5);
-	if (!line)
-		printf("TIMEOUT!\n");
-	else
-		printf("read line `%s`\n", line);
-	free(line);
+	const char *entry = NULL;
+retry:
+	for (int selectedEntry = wait_for_boot_entry(timeout); ;
+	     selectedEntry = wait_for_boot_entry(0)) {
+		timeout = 0;
+		if (selectedEntry >= 0 && selectedEntry < entryNum) {
+			entry = get_entry(cfg, selectedEntry);
+			break;
+		}
+	}
 
+	char *sKernel = get_pair(entry, "kernel");
+	if (!sKernel) {
+		pr_err("No kernel defined for the entry!\n");
+		goto retry;
+	}
 
-	void *rawImage = NULL;
-	int64_t imageSize = file_load("vmlinux-riscv", &rawImage);
-	if (imageSize < 0)
-		printf("failed to read the image\n");
+	void *rawKernel = NULL;
+	int64_t kernelSize = file_load(sKernel, &rawKernel);
+	free(sKernel);
+
+	if (kernelSize < 0) {
+		pr_err("Can't load kernel\n");
+		goto retry;
+	}
 
 	Efi_Memory_Mapped_Device_Path devpath[2];
 	devpath[0].head.type	= EFI_DEVICE_HARDWARE;
 	devpath[0].head.subtype	= EFI_DEVICE_SUBTYPE_MEMORY_MAPPED;
 	devpath[0].head.length	= sizeof(devpath[0]);
 	devpath[0].memtype	= EFI_LOADER_DATA;
-	devpath[0].startAddr	= (uint64_t)rawImage;
-	devpath[0].endAddr	= (uint64_t)rawImage + imageSize;
+	devpath[0].startAddr	= (uint64_t)rawKernel;
+	devpath[0].endAddr	= (uint64_t)rawKernel + kernelSize;
 
 	devpath[1].head.type	= EFI_DEVICE_END;
 	devpath[1].head.subtype	= EFI_DEVICE_SUBTYPE_ENTIRE_END;
 	devpath[1].head.length	= sizeof(Efi_Device_Path_Protocol);
 
-	Efi_Handle newImage = NULL;
-	pr_info("%s: parent = 0x%lx, devpath = 0x%lx, rawImage = 0x%lx\n",
-		__func__, gSelf, devpath, rawImage);
-	pr_info("%s: imageSize = %lu, &newImage = 0x%lx\n",
-		__func__, imageSize, &newImage);
+	Efi_Handle nextImage = NULL;
 	int ret = efi_call(gBS->loadImage, 0, gSelf,
 					   (Efi_Device_Path_Protocol *)devpath,
-					   rawImage, imageSize, &newImage);
+					   rawKernel, kernelSize, &nextImage);
 	if (ret != EFI_SUCCESS)
-		printf("failed to load the image, ret = %d\n", ret);
+		panic("Can't load kernel image!\n");
 
-	efi_call(gBS->startImage, newImage, 0, NULL);
-
-	return EFI_SUCCESS;
+	return efi_call(gBS->startImage, nextImage, 0, NULL);
 }
