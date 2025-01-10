cc133c819da794572b4e75a11e1331dcd8c37a52
Yao Zi <ziyao@disroot.org>
2025-01-10 08:02:04 +0000

    main: Improve boot logic and resource handling
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- src/loli.c
+++ src/loli.c
@@ -26,6 +26,10 @@ printn(const char *p, size_t len)
 		printf("%c", *(p++));
 }
 
+typedef struct {
+	Efi_Handle kernelHandle;
+} Boot_Entry;
+
 static char *
 get_pair(const char *cfg, const char *key)
 {
@@ -66,8 +70,10 @@ wait_for_boot_entry(int timeout)
 	char *line = getline_timeout(timeout);
 
 	/* TODO: support "default" option */
-	if (!line)
+	if (!line) {
+		printf("\nBoot entry 0 by default\n");
 		return 0;
+	}
 
 	int entry = atou(line);
 	free(line);
@@ -84,6 +90,61 @@ get_entry(const char *cfg, int index)
 		entry = extlinux_next_entry(NULL, entry);
 
 	return entry;
+}
+
+static int
+load_efi_image(Boot_Entry *entry, void *kernelBase, int64_t kernelSize)
+{
+	Efi_Memory_Mapped_Device_Path devpath[2];
+	devpath[0].head.type	= EFI_DEVICE_HARDWARE;
+	devpath[0].head.subtype	= EFI_DEVICE_SUBTYPE_MEMORY_MAPPED;
+	devpath[0].head.length	= sizeof(devpath[0]);
+	devpath[0].memtype	= EFI_LOADER_DATA;
+	devpath[0].startAddr	= (uint64_t)kernelBase;
+	devpath[0].endAddr	= (uint64_t)kernelBase + kernelSize;
+
+	devpath[1].head.type	= EFI_DEVICE_END;
+	devpath[1].head.subtype	= EFI_DEVICE_SUBTYPE_ENTIRE_END;
+	devpath[1].head.length	= sizeof(Efi_Device_Path_Protocol);
+
+	return efi_call(gBS->loadImage, 0, gSelf,
+			(Efi_Device_Path_Protocol *)devpath,
+			kernelBase, kernelSize,
+			&entry->kernelHandle) != EFI_SUCCESS;
+}
+
+static int
+load_and_validate_entry(const char *p, Boot_Entry *entry)
+{
+	char *kernel = get_pair(p, "kernel");
+	if (!kernel) {
+		pr_err("No kernel defined for the entry!\n");
+		goto out_err;
+	}
+
+	// We release the original kernel image after everything setups up
+	void *kernelBase = NULL;
+	int64_t kernelSize = file_load(kernel, &kernelBase);
+
+	if (kernelSize < 0) {
+		pr_err("Can't load kernel %s\n", kernel);
+		goto free_kernel;
+	}
+
+	pr_info("Kernel: %s, size = %lu\n", kernel, kernelSize);
+
+	if (load_efi_image(entry, kernelBase, kernelSize)) {
+		pr_err("Can't load kernel %s\n", kernel);
+		goto free_kernel;
+	}
+
+	free(kernelBase);
+
+	return 0;
+free_kernel:
+	free(kernelBase);
+out_err:
+	return -1;
 }
 
 Efi_Status
@@ -108,7 +169,7 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 	     p;
 	     p = extlinux_next_entry(NULL, p)) {
 		size_t namelen = 0;
-		const char *name = extlinux_get_value(cfg, "label", &namelen);
+		const char *name = extlinux_get_value(p, "label", &namelen);
 
 		printf("%d: ", entryNum);
 		printn(name, namelen);
@@ -118,49 +179,22 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 	}
 
 	const char *entry = NULL;
+	Boot_Entry bootEntry = { NULL };
 retry:
 	for (int selectedEntry = wait_for_boot_entry(timeout); ;
 	     selectedEntry = wait_for_boot_entry(0)) {
 		timeout = 0;
+		if (!entryNum)
+			continue;
+
 		if (selectedEntry >= 0 && selectedEntry < entryNum) {
 			entry = get_entry(cfg, selectedEntry);
-			break;
+			if (!load_and_validate_entry(entry, &bootEntry))
+				break;
+			else
+				pr_err("Invalid entry\n");
 		}
 	}
 
-	char *sKernel = get_pair(entry, "kernel");
-	if (!sKernel) {
-		pr_err("No kernel defined for the entry!\n");
-		goto retry;
-	}
-
-	void *rawKernel = NULL;
-	int64_t kernelSize = file_load(sKernel, &rawKernel);
-	free(sKernel);
-
-	if (kernelSize < 0) {
-		pr_err("Can't load kernel\n");
-		goto retry;
-	}
-
-	Efi_Memory_Mapped_Device_Path devpath[2];
-	devpath[0].head.type	= EFI_DEVICE_HARDWARE;
-	devpath[0].head.subtype	= EFI_DEVICE_SUBTYPE_MEMORY_MAPPED;
-	devpath[0].head.length	= sizeof(devpath[0]);
-	devpath[0].memtype	= EFI_LOADER_DATA;
-	devpath[0].startAddr	= (uint64_t)rawKernel;
-	devpath[0].endAddr	= (uint64_t)rawKernel + kernelSize;
-
-	devpath[1].head.type	= EFI_DEVICE_END;
-	devpath[1].head.subtype	= EFI_DEVICE_SUBTYPE_ENTIRE_END;
-	devpath[1].head.length	= sizeof(Efi_Device_Path_Protocol);
-
-	Efi_Handle nextImage = NULL;
-	int ret = efi_call(gBS->loadImage, 0, gSelf,
-					   (Efi_Device_Path_Protocol *)devpath,
-					   rawKernel, kernelSize, &nextImage);
-	if (ret != EFI_SUCCESS)
-		panic("Can't load kernel image!\n");
-
-	return efi_call(gBS->startImage, nextImage, 0, NULL);
+	return efi_call(gBS->startImage, bootEntry.kernelHandle, 0, NULL);
 }
