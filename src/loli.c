ziyao233/loli-loader

commit 5f53ec308c9259918b6ae7a92900d3a77da35d8b

main: Support loading initramfs ("initrd" pair)
In this commit, we introduce dt-related stuff to make sure memory
mappings could be correctly passed to the kernel. We also provide
wrappers for page-based allocators, since the kernel and initramfs are
required to aligned to 4KiB (UEFI page size).

Changes include,

- efi: complete definition of system table
- efiboot: complete definition of boot services, define locateHandle,
  introduce efi_install_configuration_table
- efidevicetree: New, U-boot private protocol for devicetree manipulation.
- fdt: Define FDT header
- string: introduce memcmp()
- memory: introduce malloc/free_pages() for page (de)allocation

Signed-off-by: Yao Zi <ziyao@disroot.org>

src/loli.c
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <extlinux.h>
 #include <misc.h>
+#include <efidevicetree.h>
+#include <fdt.h>
 
 #define LOLI_CFG "loli.cfg"
 
@@ -132,6 +134,23 @@ load_efi_image(Boot_Entry *entry, void *kernelBase, int64_t kernelSize)
 			&entry->kernelHandle) != EFI_SUCCESS;
 }
 
+static void
+setup_initrd(void *base, int64_t size)
+{
+	struct {
+		uint_native base;
+		uint_native size;
+	} *initrd = malloc(sizeof(*initrd));
+
+	initrd->base = (uint_native)base;
+	initrd->size = (uint_native)size;
+
+	efi_install_configuration_table(
+		EFI_GUID(0x5568e427, 0x68fc, 0x4f3d,
+			 0xac, 0x74, 0xca, 0x55, 0x52, 0x31, 0xcc, 0x68),
+		initrd);
+}
+
 static int
 load_and_validate_entry(const char *p, Boot_Entry *entry)
 {
@@ -142,19 +161,51 @@ load_and_validate_entry(const char *p, Boot_Entry *entry)
 		goto out_err;
 	}
 
-	void *kernelBase = NULL;
-	int64_t kernelSize = file_load(kernel, &kernelBase);
-
+	int64_t kernelSize = file_get_size(kernel);
 	if (kernelSize < 0) {
+		pr_err("Can't load kernel %s\n", kernel);
+		goto out_err;
+	}
+
+	void *kernelBase = malloc_pages(kernelSize);
+	int64_t ret = file_load(kernel, &kernelBase);
+	if (ret < 0) {
 		pr_err("Can't load kernel %s\n", kernel);
 		goto free_kernel;
 	}
 
-	pr_info("Kernel: %s, size = %lu\n", kernel, kernelSize);
-
 	if (load_efi_image(entry, kernelBase, kernelSize)) {
 		pr_err("Can't load kernel %s\n", kernel);
 		goto free_kernel_base;
+	}
+
+	pr_info("Kernel %s, size = %lu\n", kernel, kernelSize);
+
+	char *initrd = get_pair(p, "initrd");
+	if (initrd) {
+#define MiB	1024 * 1024
+		int64_t initrdSize = file_get_size(initrd);
+		if (initrdSize < 0) {
+			pr_err("Can't load initrd %s\n", initrd);
+			goto unload_image;
+		}
+
+		/*
+		 *	We reserve 2MiB memory ahead the initrd start and
+		 *	2MiB after its end, to avoid RISC-V kernel considers
+		 *	kernel and initrd overlaps.
+		 */
+		void *initrdBase = malloc_pages(initrdSize + 4 * MiB);
+		initrdBase = (char *)initrdBase + 2 * MiB;
+		initrdSize = file_load(initrd, &initrdBase);
+		if (initrdSize < 0) {
+			pr_err("Can't load initrd %s\n", initrd);
+			goto unload_image;
+		}
+
+		setup_initrd(initrdBase, initrdSize);
+	} else {
+		pr_info("Initrd: (none)\n");
 	}
 
 	char *append = get_pair(p, "append");
@@ -163,16 +214,79 @@ load_and_validate_entry(const char *p, Boot_Entry *entry)
 	setup_append(entry->kernelHandle, append);
 
 	free(kernel);
-	free(kernelBase);
+	free_pages(kernelBase, kernelSize);
 	free(append);
 
 	return 0;
+unload_image:
+	efi_call(gBS->unloadImage, entry->kernelHandle);
 free_kernel_base:
-	free(kernelBase);
+	free_pages(kernelBase, kernelSize);
 free_kernel:
 	free(kernel);
 out_err:
 	return -1;
+}
+
+static Fdt_Header *
+search_for_devicetree(size_t *size)
+{
+	Fdt_Header *fdt = NULL;
+
+	for (uint_native i = 0; i < gST->numberOfTableEntries; i++) {
+		Efi_Configuration_Table *t = gST->configurationTable + i;
+
+		Efi_Guid dtbGuid = EFI_DTB_TABLE_GUID;
+		if (!memcmp(&dtbGuid, &t->vendorGuid, sizeof(Efi_Guid))) {
+			fdt = t->vendorTable;
+			break;
+		}
+	}
+
+	if (!fdt)
+		return NULL;
+
+	/* TODO: check compatibility */
+	*size = fdt->totalSize;
+	return fdt;
+}
+
+static void
+setup_dt(void)
+{
+	size_t fdtSize = 0;
+	Fdt_Header *fdt = search_for_devicetree(&fdtSize);
+
+	if (!fdt) {
+		pr_info("DeviceTree: not found\n");
+		return;
+	}
+
+	pr_info("devicetree: found, size %lu\n", fdtSize);
+
+	/* TODO: don't use a hard size limit */
+	size_t copySize = fdtSize + 4096;
+	Fdt_Header *copy = malloc(copySize);
+	memcpy(copy, fdt, fdtSize);
+
+	Efi_Dt_Fixup_Protocol *dtp = NULL;
+	Efi_Guid dtFixupGuid = EFI_DT_FIXUP_PROTOCOL_GUID;
+	int ret = efi_call(gBS->locateProtocol, &dtFixupGuid, NULL, (void **)&dtp);
+	if (!dtp) {
+		free(copy);
+		pr_info("EFI_DT_FIXUP_PROTOCOL isn't supported, "
+			"apply no fixup\n");
+		return;
+	}
+
+	ret = efi_method(dtp, fixup, copy, &copySize,
+					 EFI_DT_APPLY_FIXES | EFI_DT_RESERVE_MEMORY);
+	if (ret == EFI_SUCCESS)
+		pr_info("devicetree: applied fixes\n");
+	else
+		pr_info("devicetree: failed to apply fixes\n");
+
+	efi_install_configuration_table(EFI_DTB_TABLE_GUID, copy);
 }
 
 Efi_Status
@@ -190,6 +304,7 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 	cfgSize = file_load(LOLI_CFG, (void **)&cfg);
 	if (cfgSize < 0)
 		panic("Can't load configuration");
+	cfg[cfgSize] = '\0';
 
 	int timeout = retrieve_timeout(cfg);
 	int entryNum = 0;
@@ -208,7 +323,6 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 
 	const char *entry = NULL;
 	Boot_Entry bootEntry = { NULL };
-retry:
 	for (int selectedEntry = wait_for_boot_entry(timeout); ;
 	     selectedEntry = wait_for_boot_entry(0)) {
 		timeout = 0;
@@ -223,6 +337,8 @@ retry:
 				pr_err("Invalid entry\n");
 		}
 	}
+
+	setup_dt();
 
 	return efi_call(gBS->startImage, bootEntry.kernelHandle, 0, NULL);
 }

include/efi.h
@@ -14,6 +14,15 @@
 
 #pragma pack(push, 0)
 
+#define EFI_DTB_TABLE_GUID \
+	EFI_GUID(0xb1b621d5, 0xf19c, 0x41a5,				\
+		 0x83, 0x0b, 0xd9, 0x15, 0x2c, 0x69, 0xaa, 0xe0)
+
+typedef struct {
+	Efi_Guid vendorGuid;
+	void *vendorTable;
+} Efi_Configuration_Table;
+
 typedef struct Efi_System_Table {
 	Efi_Table_Header header;
 
@@ -27,6 +36,9 @@ typedef struct Efi_System_Table {
 	Efi_Simple_Text_Output_Protocol *stdErr;
 	Efi_Handle runtimeServices;
 	Efi_Boot_Services *bootServices;
+
+	uint_native numberOfTableEntries;
+	Efi_Configuration_Table *configurationTable;
 } Efi_System_Table;
 
 #pragma pack(pop)

include/efiboot.h
@@ -87,7 +87,7 @@ typedef struct {
 	Efi_Handle registerProtocolNotify;
 	Efi_Handle locateHandle;
 	Efi_Handle locateDevicePath;
-	Efi_Handle installConfigurationTable;
+	Efi_Status (*installConfigurationTable)(Efi_Guid *guid, void *table);
 
 	Efi_Status (*loadImage)(bool bootPolicy, Efi_Handle parentHandle,
 				Efi_Device_Path_Protocol *devicePath,
@@ -99,6 +99,24 @@ typedef struct {
 	Efi_Handle exit;
 	Efi_Status (*unloadImage)(Efi_Handle imageHandle);
 	Efi_Handle exitBootServices;
+
+	Efi_Handle getNextMonotonicCount;
+	Efi_Handle stall;
+	Efi_Handle setWatchdogTimer;
+
+	Efi_Handle connectController;
+	Efi_Handle disconnectController;
+
+	Efi_Handle openProtocol;
+	Efi_Handle closeProtocol;
+	Efi_Handle openProtocolInformation;
+
+	Efi_Handle protocolsPerHandle;
+	Efi_Handle locateHandleBuffer;
+	Efi_Status (*locateProtocol)(Efi_Guid *protocol, void *registration,
+				     void **interfaces);
+	Efi_Handle installMultipleProtocolInterfaces;
+	Efi_Handle uninstallMultipleProtocolInterfaces;
 } Efi_Boot_Services;
 
 #pragma pack(pop)
@@ -107,6 +125,11 @@ typedef struct {
 	Efi_Guid _guid = protocol;				\
 	efi_call(gBS->handleProtocol, handle, &_guid,		\
 		 (void **)(interface));				\
+} while (0)
+
+#define efi_install_configuration_table(guid, table) do { \
+	Efi_Guid _guid = guid;						\
+	efi_call(gBS->installConfigurationTable, &_guid, table);	\
 } while (0)
 
 #endif	// __LOLI_EFIBOOT_H_INC__

include/efidevicetree.h
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/efidevicetree.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#ifndef __LOLI_DEVICETREE_H_INC__
+#define __LOLI_DEVICETREE_H_INC__
+
+#include <efidef.h>
+
+#define EFI_DT_FIXUP_PROTOCOL_GUID \
+	EFI_GUID(0xe617d64c, 0xfe08, 0x46da,				\
+		 0xf4, 0xdc, 0xbb, 0xd5, 0x87, 0x0c, 0x73, 0x00)
+
+#define EFI_DT_APPLY_FIXES		0x1
+#define EFI_DT_RESERVE_MEMORY		0x2
+
+#pragma pack(push, 0)
+
+typedef struct Efi_Dt_Fixup_Protocol {
+	uint64_t revision;
+	Efi_Status (*fixup)(struct Efi_Dt_Fixup_Protocol *self,
+			    void *fdt,
+			    uint_native *bufSize,
+			    uint32_t flags);
+} Efi_Dt_Fixup_Protocol;
+
+#pragma pack(pop)
+
+#endif	// __LOLI_DEVICETREE_H_INC__

include/fdt.h
@@ -1,0 +1,27 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/fdt.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+
+#ifndef __LOLI_FDT_H_INC__
+#define __LOLI_FDT_H_INC__
+
+#include <efidef.h>
+
+#pragma pack(push, 0)
+
+typedef struct {
+	uint32_t magic;
+	uint32_t totalSize;
+	uint32_t offDtStruct;
+	uint32_t offDtStrings;
+	uint32_t offMapRsvMap;
+	uint32_t version;
+	uint32_t lastCompVersion;
+} Fdt_Header;
+
+#pragma pack(pop)
+
+#endif	// __LOLI_FDT_H_INC__

include/memory.h
@@ -16,4 +16,6 @@ void *malloc(size_t s);
 void free(void *p);
 void *realloc(void *p, size_t oldSize, size_t size);
 
+void *malloc_pages(size_t size);
+void free_pages(void *p, size_t size);
 #endif	// __LOLI_MEMORY_H_INC__

include/string.h
@@ -30,4 +30,6 @@ int atou(const char *p);
 
 size_t strscpy(char *dst, const char *src, size_t len);
 
+int memcmp(void *a, void *b, size_t len);
+
 #endif	// __LOLI_STRING_H_INC__

src/memory.c
@@ -38,3 +38,22 @@ realloc(void *p, size_t oldSize, size_t size)
 	free(p);
 	return new;
 }
+
+#define SIZE_TO_EFI_PAGES(size) ((size + 4095) >> 12)
+
+void *
+malloc_pages(size_t size)
+{
+	void *addr;
+	int ret = efi_call(gBS->allocatePages, 0, EFI_LOADER_DATA,
+			   SIZE_TO_EFI_PAGES(size),
+			   &addr);
+	return ret == EFI_SUCCESS ? addr : NULL;
+}
+
+void
+free_pages(void *p, size_t size)
+{
+	if (p)
+		efi_call(gBS->freePages, p, SIZE_TO_EFI_PAGES(size));
+}

commit 2279a425df2d551e406a47f863b014de1a5a81e4

main: Don't free kernel memory even after loading it
Though UEFI-Specification states,

	 If SourceBuffer is not NULL, the function is a memory-to-memory
	 load in which SourceBuffer points to the image to be loaded and
	 SourceSize indicates the image's size in bytes. In this case,
	 the caller has copied the image into SourceBuffer and can free
	 the buffer once loading is complete.

But this doesn't seem to be true when using the EDK II firmware shipped
by qemu-system, where releasing the image (kernelBase) after loadImage
seems to cause overlapping between initrd and kernel image.

Fixes: 5f53ec308c92 ("main: Support loading initramfs ("initrd" pair)")
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/loli.c
@@ -216,7 +216,6 @@ load_and_validate_entry(const char *p, Boot_Entry *entry)
 	setup_append(entry->kernelHandle, append);
 
 	free(kernel);
-	free_pages(kernelBase, kernelSize);
 	free(append);
 
 	return 0;