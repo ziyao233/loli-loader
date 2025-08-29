ziyao233/loli-loader

commit 9bc79db1842bfc82c47d1055c3624b1a0c31e44e

graphics: Add basic routine for initialzing GOP
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/graphics.c
@@ -1,0 +1,73 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/src/graphics.c
+ */
+
+#include <efi.h>
+#include <eficall.h>
+#include <efiboot.h>
+#include <eficon.h>
+#include <string.h>
+#include <misc.h>
+
+static int gGraphicsAvailable;
+static uint32_t *gFrameBuffer;
+Efi_Graphics_Output_Mode_Info gFrameBufferInfo;
+
+#define MIN_HORIZONTAL_RESOLUTION	(80 * 8)
+#define MIN_VERTICAL_RESOLUTION		(24 * 16)
+
+void
+graphics_init(void)
+{
+	Efi_Guid gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	Efi_Graphics_Output_Protocol *gop = NULL;
+	Efi_Status ret;
+
+	ret = efi_call(gBS->locateProtocol, &gopGuid, NULL, (void **)&gop);
+	ret = EFI_ERRNO(ret);
+	if (ret == EFI_NOT_FOUND) {
+		pr_info("Skip graphics initialization: GOP isn't supported\r\n");
+		return;
+	} else if (ret) {
+		pr_err("locateProtocol returns %lu for GOP\n", ret);
+		panic("Failed to locate GOP handle");
+	}
+
+	uint32_t mode;
+	Efi_Graphics_Output_Mode_Info *info = NULL;
+	for (mode = 0; mode < gop->mode->maxMode; mode++) {
+		uint_native size;
+		ret = efi_method(gop, queryMode, mode, &size, &info);
+		if (ret) {
+			pr_err("Failed to query GOP mode %u: %d\r\n", mode, ret);
+			return;
+		}
+
+		if (info->pixelFormat != PIXEL_RGB_RESERVED_8888 &&
+		    info->pixelFormat != PIXEL_BGR_RESERVED_8888)
+			continue;
+
+		if (info->horizontalRes >= MIN_HORIZONTAL_RESOLUTION &&
+		    info->verticalRes >= MIN_VERTICAL_RESOLUTION)
+			break;
+	}
+
+	ret = efi_method(gop, setMode, mode);
+	if (ret) {
+		pr_err("Failed to setup GOP mode %u: %d\r\n", mode, ret);
+		return;
+	}
+
+	pr_info("Graphics initialized: GOP mode = %u\r\n", mode);
+	pr_info("Resolution %ux%u\r\n",
+		info->horizontalRes, info->verticalRes);
+
+	gFrameBuffer = gop->mode->fbBase;
+	memset(gFrameBuffer, 0, gop->mode->fbSize);
+
+	memcpy(&gFrameBufferInfo, info, sizeof(*info));
+
+	gGraphicsAvailable = 1;
+}

Makefile
@@ -22,7 +22,7 @@ MYLDFLAGS	= $(LDFLAGS)
 
 OBJS		= src/loli.o src/efi.o src/string.o src/interaction.o
 OBJS		+= src/memory.o src/file.o src/misc.o src/extlinux.o
-OBJS		+= src/eficall.o src/entry.o
+OBJS		+= src/eficall.o src/entry.o src/graphics.o
 
 default: loli.efi
 

include/eficon.h
@@ -9,6 +9,10 @@
 
 #include <efidef.h>
 
+#define EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID \
+	EFI_GUID(0x9042a9de, 0x23dc, 0x4a38,				\
+		 0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a)
+
 #pragma pack(push, 0)
 
 typedef struct {
@@ -30,6 +34,49 @@ typedef struct Efi_Simple_Text_Output_Protocol {
 	Efi_Status (*outputString)(struct Efi_Simple_Text_Output_Protocol *p,
 				   wchar_t *str);
 } Efi_Simple_Text_Output_Protocol;
+
+typedef struct Efi_Pixel_Bitmask {
+	uint32_t redMask;
+	uint32_t greenMask;
+	uint32_t blueMask;
+	uint32_t reservedMask;
+} Efi_Pixel_Bitmask;
+
+typedef enum {
+	PIXEL_RGB_RESERVED_8888 = 0,
+	PIXEL_BGR_RESERVED_8888 = 1,
+	PIXEL_BIT_MASK = 2,
+	PIXEL_BIT_ONLY = 3,
+} Efi_Graphics_Pixel_Format;
+
+typedef struct Efi_Graphics_Output_Mode_Info {
+	uint32_t version;
+	uint32_t horizontalRes;
+	uint32_t verticalRes;
+	Efi_Graphics_Pixel_Format pixelFormat;
+	Efi_Pixel_Bitmask pixelInfo;
+	uint32_t pixelPerScanline;
+} Efi_Graphics_Output_Mode_Info;
+
+typedef struct Efi_Graphics_Output_Mode {
+	uint32_t maxMode;
+	uint32_t mode;
+	Efi_Graphics_Output_Mode_Info *info;
+	uint_native sizeOfInfo;
+	void *fbBase;
+	uint_native fbSize;
+} Efi_Graphics_Output_Mode;
+
+typedef struct Efi_Graphics_Output_Protocol {
+	Efi_Status (*queryMode)(struct Efi_Graphics_Output_Protocol *p,
+				uint32_t modeNum,
+				uint_native *sizeOfInfo,
+				Efi_Graphics_Output_Mode_Info **info);
+	Efi_Status (*setMode)(struct Efi_Graphics_Output_Protocol *p,
+			      uint32_t modeNum);
+	Efi_Handle blt;
+	Efi_Graphics_Output_Mode *mode;
+} Efi_Graphics_Output_Protocol;
 
 #pragma pack(pop)
 

include/efidef.h
@@ -51,8 +51,12 @@ typedef struct {
 		(c) & 0xff, ((c) >> 8) & 0xff,				\
 		(d0), (d1), (d2), (d3), (d4), (d5), (d6), (d7) } }
 
+#define EFI_ERRNO(x)			((x) & ~(1ULL << 63))
+
 #define EFI_SUCCESS			0
 #define EFI_INVALID_PARAMETER		2
+#define EFI_BUFFER_TOO_SMALL		5
 #define EFI_OUT_OF_RESOURCES		9
+#define EFI_NOT_FOUND			14
 
 #endif	// __LOLI_EFIDEF_H_INC__

include/graphics.h
@@ -1,0 +1,12 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/graphics.h
+ */
+
+#ifndef __LOLI_GRAPHICS_H_INC__
+#define __LOLI_GRAPHICS_H_INC__
+
+void graphics_init(void);
+
+#endif // __LOLI_GRAPHICS_H_INC__

src/loli.c
@@ -18,6 +18,7 @@
 #include <misc.h>
 #include <efidevicetree.h>
 #include <fdt.h>
+#include <graphics.h>
 
 #define LOLI_CFG "loli.cfg"
 
@@ -296,6 +297,8 @@ main(Efi_Handle imageHandle, Efi_System_Table *st)
 	efi_init(imageHandle, st);
 
 	printf("loli bootloader (%s built)\r\n", __DATE__);
+
+	graphics_init();
 
 	int64_t cfgSize = file_get_size(LOLI_CFG);
 	if (cfgSize < 0)

commit 7b6ad2f8db4b4aa3a2528ce88c926e184da0fa84

graphics: Fail gracefully when no suitable GOP mode is found
Currently if no suitable GOP mode is found, we'll try to set GOP mode
to the maximum mode + 1, which will definitely fail with a confusing
message indicating GOP mode setup fails.

Let's check whether a suitable mode is found before setup it, and print
a friendly message if not.

Fixes: 9bc79db1842b ("graphics: Add basic routine for initialzing GOP")
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/graphics.c
@@ -138,6 +138,11 @@ graphics_init(void)
 			break;
 	}
 
+	if (mode == gop->mode->maxMode) {
+		pr_info("Skip graphics initialization: No suitable mode\n");
+		return;
+	}
+
 	ret = efi_method(gop, setMode, mode);
 	if (ret) {
 		pr_err("Failed to setup GOP mode %u: %d\n", mode, ret);