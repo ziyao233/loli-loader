15db4680ff68e01858daceb0e9436c5586692ecb
Yao Zi <ziyao@disroot.org>
2025-10-06 14:10:32 +0000

    eficall: Add EFI ABI wrapper with 10 arguments
    Blit function of UEFI GOP requires 10 arguments.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- include/eficall.h
+++ include/eficall.h
@@ -19,6 +19,7 @@ int __efi_call3(unsigned long int, ...);
 int __efi_call4(unsigned long int, ...);
 int __efi_call5(unsigned long int, ...);
 int __efi_call6(unsigned long int, ...);
+int __efi_call10(unsigned long int, ...);
 
 #define __arg(x) ((unsigned long int)(x))
 #define _efi_call1(x, a) \
@@ -33,9 +34,13 @@ int __efi_call6(unsigned long int, ...);
 	__efi_call5(__arg(a), __arg(b), __arg(c), __arg(d), __arg(e), __arg(x))
 #define _efi_call6(x, a, b, c, d, e, f) \
 	__efi_call6(__arg(a), __arg(b), __arg(c), __arg(d), __arg(e), __arg(f), __arg(x))
+#define _efi_call10(x, a, b, c, d, e, f, g, h, i, j) \
+	__efi_call10(__arg(a), __arg(b), __arg(c), __arg(d), __arg(e),	\
+		     __arg(f), __arg(g), __arg(h), __arg(i), __arg(j), __arg(x))
 
-#define __efi_call_nargs(a, b, c, d, e, f, n, ...) n
-#define efi_call_nargs(...) __efi_call_nargs(__VA_ARGS__, 6, 5, 4, 3, 2, 1,)
+#define __efi_call_nargs(a, b, c, d, e, f, g, h, i, j, n, ...) n
+#define efi_call_nargs(...) \
+	__efi_call_nargs(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,)
 
 #define __efi_call_concat(a, b) a##b
 #define efi_call_concat(a, b) __efi_call_concat(a, b)
