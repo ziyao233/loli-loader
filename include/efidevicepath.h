79ecb565e6fb36ffe93792bd4fb55c3eab8ddaef
Yao Zi <ziyao@disroot.org>
2025-10-12 13:57:39 +0000

    include/efidevicepath: Add definitons for EFI_DEVICE_SUBTYPE_VENDOR
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- include/efidevicepath.h
+++ include/efidevicepath.h
@@ -10,6 +10,10 @@
 
 #include <efidef.h>
 
+#define EFI_DEVICE_PATH_PROTOCOL_GUID \
+	EFI_GUID(0x09576e91, 0x6d3f, 0x11d2,				\
+		 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
+
 #pragma pack(push, 0)
 
 typedef struct {
@@ -25,10 +29,19 @@ typedef struct {
 	uint64_t endAddr;
 } Efi_Memory_Mapped_Device_Path;
 
+typedef struct {
+	Efi_Device_Path_Protocol head;
+	Efi_Guid vendorGuid;
+	/* Omitted to simplify definition of device paths */
+	/* uint8_t vendorData[]; */
+} Efi_Vendor_Media_Device_Path;
+
 #pragma pack(pop)
 
 #define EFI_DEVICE_HARDWARE			1
 #define  EFI_DEVICE_SUBTYPE_MEMORY_MAPPED	6
+#define EFI_DEVICE_MEDIA			4
+#define  EFI_DEVICE_SUBTYPE_VENDOR		3
 #define EFI_DEVICE_END				0x7f
 #define  EFI_DEVICE_SUBTYPE_ENTIRE_END		0xff
 
