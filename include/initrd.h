fc7ad7887f4a1daebfb89f773383580b71c72bba
Yao Zi <ziyao@disroot.org>
2025-10-12 14:26:50 +0000

    include/initrd: Add missing header
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ include/initrd.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/initrd.h
+ *	Copyright (C) 2025 Yao Zi <ziyao@disroot.org>
+ */
+
+#ifndef __LOLI_INITRD_H_INC__
+#define __LOLI_INITRD_H_INC__
+
+int initrd_setup(void *base, size_t size);
+
+#endif	// __LOLI_INITRD_H_INC__
+
