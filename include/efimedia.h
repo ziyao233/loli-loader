c830909a09324a039240cadab16f37a91685e73e
Yao Zi <ziyao@disroot.org>
2025-10-12 13:57:12 +0000

    include/efimedia: Add bits for EFI_LOAD_FILE(2)_PROTOCOL
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- include/efimedia.h
+++ include/efimedia.h
@@ -19,7 +19,17 @@
 	EFI_GUID(0x09576e92, 0x6d3f, 0x11d2,				\
 		 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
 
+#define EFI_LOAD_FILE_PROTOCOL_GUID \
+	EFI_GUID(0x56ec3091, 0x954c, 0x11d2,				\
+		 0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
+
+#define EFI_LOAD_FILE2_PROTOCOL_GUID \
+	EFI_GUID(0x4006c0c1, 0xfcb3, 0x403e,				\
+		 0x99, 0x6d, 0x4a, 0x6c, 0x87, 0x24, 0xe0, 0x6d)
+
 #pragma pack(push, 0)
+
+struct Efi_Device_Path_Protocol;
 
 typedef struct {
 	uint64_t size;
@@ -54,6 +64,16 @@ typedef struct Efi_File_Protocol {
 			      uint_native *bufSize, void *buf);
 	Efi_Handle setInfo;
 } Efi_File_Protocol;
+
+typedef struct Efi_Load_File_Protocol {
+	Efi_Status (*loadFile)(struct Efi_Load_File_Protocol *p,
+			       Efi_Device_Path_Protocol *dp,
+			       bool bootPolicy,
+			       uint_native *bufferSize,
+			       void *buffer);
+} Efi_Load_File_Protocol;
+
+typedef Efi_Load_File_Protocol Efi_Load_File2_Protocol;
 
 #pragma pack(pop)
 
