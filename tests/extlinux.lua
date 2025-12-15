ziyao233/loli-loader

commit d6eb007023c57c675919767f7456d509b8d08520

Add extlinux parser and corresponding testsuite
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/extlinux.c
@@ -1,0 +1,127 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	src/extlinux.c
+ *	Copyright (c) 2024 Yao Zi. All rights reserved.
+ */
+
+#include <string.h>
+
+#include "extlinux.h"
+
+static int
+is_space(const char *p)
+{
+	return *p == '\t' || *p == ' ';
+}
+
+static const char *
+skip_space(const char *p)
+{
+	if (!p)
+		return NULL;
+
+	while (is_space(p))
+		p++;
+
+	return *p ? p : NULL;
+}
+
+static const char *
+is_key(const char *p, const char *key)
+{
+	while (*p && *key) {
+		if (*p != *key)
+			return NULL;
+		p++;
+		key++;
+	}
+
+	if (*key)
+		return NULL;
+
+	if (is_space(p)) {
+		p = skip_space(p);
+
+		if (*p != '\n' && *p)
+			return p;
+	}
+
+	return NULL;
+}
+
+static const char *
+next_line(const char *p)
+{
+	if (!p)
+		return NULL;
+
+	while (*p != '\n' && *p)
+		p++;
+
+	if (!*p)
+		return NULL;
+
+	p++;	// skip the '\n'
+
+	return *p ? p : NULL;
+}
+
+const char *
+extlinux_next_entry(const char *conf, const char *last)
+{
+	const char *p = conf;
+
+	/* skip the last "label" pair */
+	if (last)
+		p = is_key(last, "label");
+
+	while (p) {
+		p = skip_space(p);
+
+		if (is_key(p, "label"))
+			return p;
+
+		p = next_line(p);
+	}
+
+	return p;
+}
+
+const char *
+extlinux_get_value(const char *p, const char *key, unsigned long int *valuelen)
+{
+	int getLabel = key[0] == 'l' && key[1] == 'a' && key[2] == 'b' &&
+		       key[3] == 'e' && key[4] == 'l' && !key[5];
+
+	if (!getLabel && is_key(p, "label"))
+		p = next_line(p);
+
+	const char *value = NULL;
+	while (p) {
+		p = skip_space(p);
+		if (!p)
+
+		if (is_key(p, "label") && !getLabel)
+			break;
+
+		value = is_key(p, key);
+		if (value) {
+			const char *vend = value;
+
+			while (*vend && *vend != '\n')
+				vend++;
+			vend--;
+
+			while (vend >= value && is_space(vend))
+				vend--;
+
+			*valuelen = vend - value + 1;
+			return value;
+		}
+
+		p = next_line(p);
+	}
+
+	return NULL;
+}

tests/extlinux.lua
@@ -1,0 +1,158 @@
+-- SPDX-License-Identifier: MPL-2.0
+--[[
+--	loli-loader testsuite
+--	/tests/extlinux.lua
+--	Copyright (c) 2024 Yao Zi.
+--]]
+
+local io		= require "io";
+local string		= require "string";
+local os		= require "os";
+
+local extlinux		= require "lextlinux";
+
+local verboseLevel	= os.getenv("VERBOSE");
+
+local function
+verbose(fmt, ...)
+	if verboseLevel then
+		print(" ===> " .. string.format(fmt, ...));
+	end
+end
+
+local function
+get(iter, self, key, expect)
+	local ret = extlinux.get(iter, key);
+	verbose("get key %q", key);
+	assert(ret == expect,
+	       ("expect %q, got %q"):format(expect, ret));
+	return iter;
+end
+
+local function
+next(iter, self, expect)
+	iter = extlinux.next(iter, key);
+
+	verbose("find next entry, expect %s", expect and expect or "not found");
+
+	if not expect then
+		assert(not iter);
+		return nil;
+	end
+
+	assert(iter);
+
+	assert(extlinux.get(iter, "label") == expect,
+	       ("expect label to be %q, got %q"):format(expect, ret));
+	return iter;
+end
+
+local cases = {
+	{
+		name	= "example",
+		conf	= [[
+timeout 10
+label Linux 6.6
+	kernel  /Image-6.6
+	initrd  /initramfs-6.6.img
+	append  console=tty0 quiet
+label Linux 6.12
+	kernel  /Image-6.12
+	initrd  /initramfs-6.12.img
+	append  console=tty0 quiet
+]],
+		opts	= {
+			{ get, "timeout", "10" },
+			{ get, "default", nil },
+			{ next, "Linux 6.6" },
+			{ get, "kernel", "/Image-6.6" },
+		},
+	},
+	{
+		name	= "no entries",
+		conf	= [[
+timeout 10
+default 1
+
+]],
+		opts	= {
+			{ get, "timeout", "10" },
+			{ get, "default", "1" },
+			{ next, nil },
+		},
+	},
+	{
+		name	= "no global options",
+		conf	= [[
+label test1
+	kernel image1
+label test2
+	kernel image2
+]],
+		opts	= {
+			{ get, "timeout", nil },
+			{ get, "default", nil },
+			{ next, "test1" },
+			{ get, "kernel", "image1" },
+			{ next, "test2" },
+			{ get, "kernel", "image2" },
+			{ next, nil },
+		},
+	},
+	{
+		--[[
+		--	We consider pairs without a value malformed, but they
+		--	shouldn't break parsing on other lines
+		--]]
+		name	= "pairs without value",
+		conf	= [[
+timeout 10
+default 1
+empty
+empty2	
+quiet 0
+label test
+	empty
+	append quiet
+]],
+		opts	= {
+			{ get, "timeout", "10" },
+			{ get, "empty", nil },
+			{ get, "empty2", nil },
+			{ get, "quiet", "0" },
+			{ next, "test", "0" },
+			{ get, "empty", nil },
+			{ get, "append", "quiet" },
+			{ next, nil },
+		},
+	},
+	{
+		name	= "missing newline at EOF",
+		conf	= [[
+label test1
+kernel test1]],
+		opts	= {
+			{ next, "test1" },
+			{ get, "kernel", "test1" },
+			{ next, nil },
+		},
+	}
+};
+
+local function
+runCase(conf, opts)
+	local iter = conf;
+
+	for _, opt in ipairs(opts) do
+		iter = opt[1](iter, table.unpack(opt));
+	end
+end
+
+for i, case in ipairs(cases) do
+	io.stdout:write(("case %d: %s "):format(i, case.name));
+	io.stdout:flush();
+
+	runCase(case.conf, case.opts);
+
+	io.stdout:write("[OK]\n");
+end

Makefile
@@ -17,7 +17,7 @@ CCASFLAGS	?= $(CFLAGS)
 LDFLAGS		=
 
 OBJS		= src/loli.o src/efi.o src/string.o src/interaction.o
-OBJS		+= src/memory.o src/file.o src/misc.o
+OBJS		+= src/memory.o src/file.o src/misc.o src/extlinux.o
 
 default: loli.efi
 

include/extlinux.h
@@ -1,0 +1,14 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ *	loli-loader
+ *	/include/extlinux.h
+ *	Copyright (c) 2024 Yao Zi.
+ */
+#ifndef __LOLI_EXTLINUX_H_INC__
+#define __LOLI_EXTLINUX_H_INC__
+
+const char *extlinux_next_entry(const char *conf, const char *last);
+const char *extlinux_get_value(const char *conf, const char *key,
+			       unsigned long int *valuelen);
+
+#endif	// __LOLI_EXTLINUX_H_INC__

src/loli.c
@@ -14,6 +14,14 @@
 #include <memory.h>
 #include <file.h>
 #include <string.h>
+#include <extlinux.h>
+
+void
+printn(const char *p, size_t len)
+{
+	while (len--)
+		printf("%c", *(p++));
+}
 
 Efi_Status
 _start(Efi_Handle imageHandle, Efi_System_Table *st)
@@ -39,8 +47,30 @@ _start(Efi_Handle imageHandle, Efi_System_Table *st)
 	char *buf = NULL;
 	int64_t size = file_load("test.txt", (void **)&buf);
 	printf("test.txt content:\n\t");
-	for (int i = 0; i < size; i++)
-		printf("%c", buf[i]);
+	printn(buf, size);
+	free(buf);
+
+	size = file_get_size("extlinux.conf");
+	printf("Length of extlinux.conf is %ld\n", size);
+	buf = malloc(size + 1);
+	file_load("extlinux.conf", (void **)&buf);
+	printf("extlinux.conf content:\n");
+	printn(buf, size);
+	buf[size] = '\0';
+	printf("extlinux.conf boot entries\n");
+	int entryNum = 0;
+	for (const char *p = extlinux_next_entry(buf, NULL);
+	     p;
+	     p = extlinux_next_entry(NULL, p)) {
+		size_t namelen = 0;
+
+		const char *name = extlinux_get_value(p, "label", &namelen);
+		printf("%d: ", entryNum + 1);
+		printn(name, namelen);
+		printf("\n");
+
+		entryNum++;
+	}
 
 	return EFI_SUCCESS;
 }

tests/extlinux.sh
@@ -1,0 +1,7 @@
+set -e
+
+cc lextlinux.c ../src/extlinux.c -shared -fPIC -o lextlinux.so \
+	-iquote../include \
+	$(pkg-config --cflags lua5.4) \
+	-Wall -Werror -pedantic -Wextra
+lua5.4 extlinux.lua

tests/lextlinux.c
@@ -1,0 +1,67 @@
+/*
+ *	loli-loader testsuite
+ *	/tests/lextlinux.c
+ *	Lua binding to extlinux.c
+ */
+
+#include "extlinux.h"
+
+#include <lua.h>
+#include <lauxlib.h>
+
+static int
+lextlinux_next(lua_State *l)
+{
+	const char *conf = NULL, *entry = NULL;
+
+	switch (lua_type(l, 1)) {
+	case LUA_TLIGHTUSERDATA:
+		entry = lua_touserdata(l, 1);
+		break;
+	case LUA_TSTRING:
+		conf = lua_tostring(l, 1);
+		break;
+	}
+
+	const char *p = extlinux_next_entry(conf, entry);
+	if (p)
+		lua_pushlightuserdata(l, (void *)p);
+
+	return p ? 1 : 0;
+}
+
+static int
+lextlinux_get(lua_State *l)
+{
+	const char *p = NULL;
+	switch (lua_type(l, 1)) {
+	case LUA_TLIGHTUSERDATA:
+		p = lua_touserdata(l, 1);
+		break;
+	case LUA_TSTRING:
+		p = lua_tostring(l, 1);
+		break;
+	}
+
+	const char *key = luaL_checkstring(l, 2);
+
+	size_t len;
+	const char *value = extlinux_get_value(p, key, &len);
+	if (value)
+		lua_pushlstring(l, value, len);
+
+	return value ? 1 : 0;
+}
+
+static const luaL_Reg extlinux_funcs[] = {
+	{ "next", lextlinux_next },
+	{ "get", lextlinux_get },
+	{ NULL, NULL },
+};
+
+int
+luaopen_lextlinux(lua_State *l)
+{
+	luaL_newlib(l, extlinux_funcs);
+	return 1;
+}

commit fb5a0ace4aac0388704e409d220401028da40276

extlinux: Skip the whole line with label when looking for the next entry
A label pair with word "label" as its value could mess up the parser
logic if we only skip the "label" key part when looking for the next
entry. Start processing from the next line instead.

Fixes: d6eb007023c5 ("Add extlinux parser and corresponding testsuite")
Signed-off-by: Yao Zi <me@ziyao.cc>

src/extlinux.c
@@ -121,7 +121,7 @@ extlinux_next_entry(const char *conf, const char *last)
 
 	/* skip the last "label" pair */
 	if (last)
-		p = match_key(last, "label");
+		p = next_line(last);
 
 	while (p) {
 		p = skip_space(p);

tests/extlinux.lua
@@ -136,7 +136,19 @@ kernel test1]],
 			{ get, "kernel", "test1" },
 			{ next, nil },
 		},
-	}
+	},
+	{
+		name	= "Word \"label\" in values",
+		conf	= [[
+label label a
+kernel label
+]],
+		opts	= {
+			{ next, "label a" },
+			{ get, "kernel", "label" },
+			{ next, nil },
+		},
+	},
 };
 
 local function