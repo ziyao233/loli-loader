ziyao233/loli-loader

commit ec0c960e78412c6d97d1633f728c9fbcbbe876ad

treewide: Support x86_64 architecture
The most important difference between x86_64 and riscv64 is that UEFI
on x86_64 doesn't use the SystemV-style calling convention, but the
Windows one.

In order to keep compatible with most of the compilers, we introduce
some hand-written assembly (eficall.S) as wrappers between SystemV ABI
and Windows ABI. The main function should also be wrapped to correctly
receive the self handle and Efi_System_Table.

For convenience, a technique used by musl's syscall wrappers are
introduced as well. eficall.h could dispatch calls with different
arguments (1 - 6) to different wrapper implementation, saving the
burden of counting arguments manually :)

Signed-off-by: Yao Zi <ziyao@disroot.org>

include/eficall.h
@@ -8,8 +8,40 @@
 #ifndef __LOLI_EFICALL_H_INC__
 #define __LOLI_EFICALL_H_INC__
 
-#ifdef LOLI_TARGET_RISCV64
+#if defined(LOLI_TARGET_RISCV64)
 #define efi_call(f, ...) ((f)(__VA_ARGS__))
+#elif defined(LOLI_TARGET_X86_64)
+
+int __efi_call1(unsigned long int, ...);
+int __efi_call2(unsigned long int, ...);
+int __efi_call3(unsigned long int, ...);
+int __efi_call4(unsigned long int, ...);
+int __efi_call5(unsigned long int, ...);
+int __efi_call6(unsigned long int, ...);
+
+#define __arg(x) ((unsigned long int)(x))
+#define _efi_call1(x, a) \
+	__efi_call1(__arg(a), __arg(x))
+#define _efi_call2(x, a, b) \
+	__efi_call2(__arg(a), __arg(b), __arg(x))
+#define _efi_call3(x, a, b, c) \
+	__efi_call3(__arg(a), __arg(b), __arg(c), __arg(x))
+#define _efi_call4(x, a, b, c, d) \
+	__efi_call4(__arg(a), __arg(b), __arg(c), __arg(d), __arg(x))
+#define _efi_call5(x, a, b, c, d, e) \
+	__efi_call5(__arg(a), __arg(b), __arg(c), __arg(d), __arg(e), __arg(x))
+#define _efi_call6(x, a, b, c, d, e, f) \
+	__efi_call6(__arg(a), __arg(b), __arg(c), __arg(d), __arg(e), __arg(f), __arg(x))
+
+#define __efi_call_nargs(a, b, c, d, e, f, n, ...) n
+#define efi_call_nargs(...) __efi_call_nargs(__VA_ARGS__, 6, 5, 4, 3, 2, 1,)
+
+#define __efi_call_concat(a, b) a##b
+#define efi_call_concat(a, b) __efi_call_concat(a, b)
+
+#define efi_call(f, ...) \
+	efi_call_concat(_efi_call,efi_call_nargs(__VA_ARGS__))(f, __VA_ARGS__)
+
 #else
 #error "Unknown target"
 #endif

Makefile
@@ -11,6 +11,7 @@ OBJCOPY		= objcopy
 PYTHON		= python
 
 CONFIG_$(ARCH)	= yes
+ARCHFLAGS_$(CONFIG_x86_64)	= -DLOLI_TARGET_X86_64
 ARCHFLAGS_$(CONFIG_riscv64)	= -DLOLI_TARGET_RISCV64
 
 MYCFLAGS	?= -ffreestanding -fno-stack-protector -fno-stack-check \
@@ -22,6 +23,7 @@ MYLDFLAGS	= $(LDFLAGS)
 
 OBJS		= src/loli.o src/efi.o src/string.o src/interaction.o
 OBJS		+= src/memory.o src/file.o src/misc.o src/extlinux.o
+OBJS		+= src/eficall.o src/entry.o
 
 default: loli.efi
 

src/eficall.S
@@ -1,0 +1,98 @@
+/*
+ *	loli-loader
+ *	src/eficall.S
+ *	Copyright (c) 2025 Yao Zi.
+ */
+
+#if LOLI_TARGET_X86_64
+
+#define STUB(n) __efi_call##n
+
+	.global STUB(1), STUB(2), STUB(3), STUB(4), STUB(5), STUB(6)
+
+/*
+ *	SystemV ABI: %rdi, %rsi, %rdx, %rcx, %r8, %r9, stack
+ *	Win32 ABI: %rcx, %rdx, %r8, %r9, stack, shadow space
+ */
+
+STUB(1):
+
+	movq		%rdi,		%rcx
+
+	addq		$-32,		%rsp
+	callq		*%rsi
+	addq		$32,		%rsp
+
+	retq
+
+STUB(2):
+	movq		%rdx,		%rax		// function pointer
+
+	movq		%rdi,		%rcx
+	movq		%rsi,		%rdx
+
+	addq		$-32,		%rsp
+	callq		*%rax
+	addq		$32,		%rsp
+
+	retq
+
+STUB(3):
+	movq		%rcx,		%rax		// function pointer
+
+	movq		%rdx,		%r8
+	movq		%rdi,		%rcx
+	movq		%rsi,		%rdx
+
+	addq		$-32,		%rsp
+	callq		*%rax
+	addq		$32,		%rsp
+
+	retq
+
+STUB(4):
+	movq		%r8,		%rax		// function pointer
+
+	movq		%rdx,		%r8
+	movq		%rcx,		%r9
+	movq		%rdi,		%rcx
+	movq		%rsi,		%rdx
+
+	addq		$-32,		%rsp
+	callq		*%rax
+	addq		$32,		%rsp
+
+	retq
+
+STUB(5):
+	movq		%r9,		%rax		// function pointer
+
+	pushq		%r8
+	movq		%rdx,		%r8
+	movq		%rcx,		%r9
+	movq		%rdi,		%rcx
+	movq		%rsi,		%rdx
+
+	addq		$-32,		%rsp
+	callq		*%rax
+	addq		$40,		%rsp
+
+	retq
+
+STUB(6):
+	movq		8(%rsp),	%rax		// function pointer
+
+	pushq		%r9
+	pushq		%r8
+	movq		%rdx,		%r8
+	movq		%rcx,		%r9
+	movq		%rdi,		%rcx
+	movq		%rsi,		%rdx
+
+	addq		$-32,		%rsp
+	callq		*%rax
+	addq		$48,		%rsp
+
+	retq
+
+#endif	// LOLI_TARGET_X86_64

src/entry.S
@@ -1,0 +1,16 @@
+/*
+ *	loli-loader
+ *	/src/entry.S
+ *	Copyright (c) 2025 Yao Zi.
+ */
+
+	.global		_start
+
+_start:
+#if defined(LOLI_TARGET_RISCV64)
+	j		main
+#elif defined(LOLI_TARGET_X86_64)
+	movq		%rcx,		%rdi
+	movq		%rdx,		%rsi
+	jmp		main
+#endif

src/loli.c
@@ -291,7 +291,7 @@ setup_dt(void)
 }
 
 Efi_Status
-_start(Efi_Handle imageHandle, Efi_System_Table *st)
+main(Efi_Handle imageHandle, Efi_System_Table *st)
 {
 	efi_init(imageHandle, st);
 

commit 5325f1ab91d442caa22e90b31c54865365a59982

eficall: Return unsigned long from wrappers
Or 64-bit return values, e.g. EFI status code, may be truncated.

Fixes: ec0c960e7841 ("treewide: Support x86_64 architecture")
Fixes: 15db4680ff68 ("eficall: Add EFI ABI wrapper with 10 arguments")
Signed-off-by: Yao Zi <me@ziyao.cc>

include/eficall.h
@@ -13,13 +13,13 @@
 #define efi_call(f, ...) ((f)(__VA_ARGS__))
 #elif defined(LOLI_TARGET_X86_64)
 
-int __efi_call1(unsigned long int, ...);
-int __efi_call2(unsigned long int, ...);
-int __efi_call3(unsigned long int, ...);
-int __efi_call4(unsigned long int, ...);
-int __efi_call5(unsigned long int, ...);
-int __efi_call6(unsigned long int, ...);
-int __efi_call10(unsigned long int, ...);
+unsigned long __efi_call1(unsigned long int, ...);
+unsigned long __efi_call2(unsigned long int, ...);
+unsigned long __efi_call3(unsigned long int, ...);
+unsigned long __efi_call4(unsigned long int, ...);
+unsigned long __efi_call5(unsigned long int, ...);
+unsigned long __efi_call6(unsigned long int, ...);
+unsigned long __efi_call10(unsigned long int, ...);
 
 #define __arg(x) ((unsigned long int)(x))
 #define _efi_call1(x, a) \